51de3a0767de833ac1653a8b602ab8aeb38ba00a
Petar-Gabriel Matev <pmatev@asteasolutions.com>
2024-01-07 01:56:51 +0200

    chore: added google login integration

--- expenses-tracker-frontend/.eslintrc.cjs
+++ expenses-tracker-frontend/.eslintrc.cjs
@@ -31,6 +31,6 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    'no-console': 'error',
+    'no-console': 'warn',
   },
 };

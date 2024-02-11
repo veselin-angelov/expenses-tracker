017630e3ca6c52d3513749834b86b8ca130873d1
Petar-Gabriel Matev <petargabriel.matev@gmail.com>
2024-02-12 00:27:53 +0200

    fix: client error

--- expenses-tracker-frontend/src/pages/Home.tsx
+++ expenses-tracker-frontend/src/pages/Home.tsx
@@ -1,14 +1,3 @@
-import { Button } from '@mui/material';
-import { useAsyncAction } from '../hooks/useAsyncAction';
-import { HttpService } from '../services/http-service';
-
 export function Home() {
-  const http = new HttpService();
-  const { trigger } = useAsyncAction(async () => await http.get('/'));
-
-  return (
-    <>
-      <h1>Hello world</h1>;<Button onClick={() => trigger()}>Click </Button>
-    </>
-  );
+  return <h1>Hello world</h1>;
 }
